25595dfd322c127a2a32e3a2a84091b8cef02a24
kbilgund <kiranbilgundi1@gmail.com>
2017-04-17 19:54:43 +0530

    Update MDIO_dut.v
    added a module template for mdio

--- MDIO_dut.v
+++ MDIO_dut.v
@@ -1,1 +1,17 @@
-
+module mdio (clk, reset,data); // currently using mdio model 
+  input      clk;
+  input      reset;
+  inout      data;
+  reg        [16:0] data;
+ // reg        [1:0] state; // 0:idle 1:read 2:write 3:error 
+  
+  always @(posedge clk or posedge reset)
+  begin
+    if (reset) begin
+      data <= 16'heeee;
+    end 
+    else begin
+      //data <= 1'b1;
+    end
+  end
+endmodule
