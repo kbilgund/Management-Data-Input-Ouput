84561b67cd5be86e980b0153a70b2fbe035d0300
kbilgund <kiranbilgundi1@gmail.com>
2017-04-17 19:38:57 +0530

    Update top.sv
    added clock and run test

--- top.sv
+++ top.sv
@@ -1,4 +1,21 @@
-//kbilgund 
-module top
+// kbilgund 
+// components needed 
+// 1. Dut instance 
+// 2. interface 
+// 3. clock generation 
 
+module top;
+import uvm_pkg::*;
+  initial
+  begin
+    run_test();
+  end
+  
+  initial begin // clock generation for tb 
+    clk = 0 ;
+    forever begin
+    #1ns clk = ~clk ;
+    end 
+  end 
+  
 endmodule 
