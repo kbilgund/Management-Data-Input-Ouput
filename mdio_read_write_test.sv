4855c1ade535ed1a92e4af1dd9312b7f5b9ee33d
kbilgund <kiranbilgundi1@gmail.com>
2017-04-24 10:26:34 +0530

    Update mdio_read_write_test.sv
    updated the test

--- mdio_read_write_test.sv
+++ mdio_read_write_test.sv
@@ -8,7 +8,7 @@ function new(string name = "mdio_test", uvm_component parent = null);
 endfunction
 
 function void build_phase(uvm_phase phase);
-  mdio_env_0 = mydio_env::type_id::create("mdio_env_0", this)
+  mdio_env_0 = mdio_env::type_id::create("mdio_env_0", this);
 endfunction: build_phase
 
 function void connect_phase(uvm_phase phase);
@@ -20,15 +20,23 @@ task run_phase(uvm_phase phase);
   bit [4:0]  reg_addr;
   bit [15:0] read_data;
   bit [15:0] write_data;
+  phase.raise_objection(this, "starting mdio read/writes");
+ 
+  wait(mdio_env_0.mdio_vif.reset == 0);
+  repeat(10) @(mdio_env_0.mdio_vif.tb_clk);
+    
+    
   repeat(10)begin 
     phy_addr   = $urandom_range(0,5); // depends on number of slave  assume 5 slaves 
     reg_addr   = $urandom();
     write_data = $urandom();
-    mdio_env_0.mdio_write(phy_addr,reg_addr,write_data);
-    mdio_env_0.mdio_read(phy_addr,reg_addr,read_data);
+    mdio_env_0.mdio_driv_0.mdio_write(phy_addr,reg_addr,write_data);
+    repeat(1) @(mdio_env_0.mdio_vif.tb_clk);
+    mdio_env_0.mdio_driv_0.mdio_read(phy_addr,reg_addr,read_data);
     if(read_data != write_data)
       `uvm_error("MDIO_TEST", "Random write data != read data")
   end 
+      phase.drop_objection(this, "finished mdio read/writes");
 endtask: run_phase
 
 endclass: mdio_test
