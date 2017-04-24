1e2f8fadffbd7bd2a0bfb870553eac0e9d94b7d1
kbilgund <kiranbilgundi1@gmail.com>
2017-04-24 10:27:48 +0530

    Update mdio_driver.sv
    updated driver

--- mdio_driver.sv
+++ mdio_driver.sv
@@ -1,8 +1,7 @@
-class mdio_driver extends uvm_driver #(mdio_seq_item);
+class mdio_driver extends uvm_driver;
 
 `uvm_component_utils(mdio_driver)
 
-mdio_seq_item req;
 
 virtual mdio_if mdio_vif;
 
@@ -13,38 +12,74 @@ endfunction
 task run_phase(uvm_phase phase);
   forever
     begin         
-      repeat(200) @(mdio_vif.tb_clk); // master should generate the clk , min 400ns as per wiki 
+      repeat(5) @(mdio_vif.tb_clk); // master should generate the clk , min 400ns as per wiki 
         mdio_vif.clk = 1;
-      repeat(200) @(mdio_vif.tb_clk);
+      repeat(5) @(mdio_vif.tb_clk);
         mdio_vif.clk = 0;
    end
 endtask: run_phase
 
 task mdio_read(input bit [4:0] phy_addr, input bit [4:0] reg_addr, output bit [15:0] data);
   
-  @(posedge mdio_vif.clk);
+  @(negedge mdio_vif.clk);
   mdio_vif.data <= 0; // start pulse 
-  @(posedge mdio_vif.clk);
+  
+  @(negedge mdio_vif.clk);
   mdio_vif.data <= 1;
   
-  @(posedge mdio_vif.clk);
+  @(negedge mdio_vif.clk);
   mdio_vif.data <= 1; // read code 
-  @(posedge mdio_vif.clk);
+  @(negedge mdio_vif.clk);
   mdio_vif.data <= 0;
-
+ 
+  repeat(2)@(posedge mdio_vif.clk);
+  mdio_vif.data <= 1'bz;
+  
+  for(int i=0;i<16;i++)begin 
+     @(posedge mdio_vif.clk);
+    data[16-i-1] = mdio_vif.data;
+  end 
+  
   
 endtask 
 
 task mdio_write(input bit [4:0] phy_addr, input bit [4:0] reg_addr, input [15:0] data);
-    
+  $display("TB write request = %h",{phy_addr,reg_addr,data});
+   
+  @(negedge mdio_vif.clk);
   mdio_vif.data <= 0; // start pulse 
-  @(posedge mdio_vif.clk);
+
+  @(negedge mdio_vif.clk);
+  mdio_vif.data <= 1;
+
+  @(negedge mdio_vif.clk);
+  mdio_vif.data <= 0; // write code 
+
+  @(negedge mdio_vif.clk);
   mdio_vif.data <= 1;
   
-  @(posedge mdio_vif.clk);
-  mdio_vif.data <= 0; // write code 
-  @(posedge mdio_vif.clk);
-  mdio_vif.data <= 1;
+  for(int i=0 ; i < 5; i++)begin 
+    @(negedge mdio_vif.clk);
+    mdio_vif.data <= phy_addr[5-i-1];
+  end 
+  
+  for(int i=0 ; i < 5; i++)begin 
+    @(negedge mdio_vif.clk);
+    mdio_vif.data <= reg_addr[5-i-1];
+  end 
+  
+  @(negedge mdio_vif.clk);
+  mdio_vif.data <= 1; // end pulse 
+
+  @(negedge mdio_vif.clk);
+  mdio_vif.data <= 0;
+  
+  for(int i=0 ; i < 16; i++)begin  // data 
+    @(negedge mdio_vif.clk);
+    mdio_vif.data <= data[16-i-1];
+  end 
+  
+   mdio_vif.data <= 1'bz;
   
 endtask
 
